c8ad8e33e82ee0b4425300d51d0ac18764afef8c
thedarkginger <jp.gooderham@gmail.com>
2014-04-04 18:38:46 -0400

    got feedback from team. added legal and other stuff back in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Onemonth::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "legal" => "pages#legal"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
